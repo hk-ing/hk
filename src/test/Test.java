8dabb46e681172986da7888f0e7ee3bf85f0c555
Hankun <2249640538@qq.com>
2022-09-27 09:54:46 +0800

    idea test的第三次提交测试

--- src/test/Test.java
+++ src/test/Test.java
@@ -8,5 +8,6 @@ public class Test {
     public static void main(String[] args) {
         System.out.println("git练习");
         System.out.println("git222练习");
+        System.out.println("git333练习");
     }
 }
